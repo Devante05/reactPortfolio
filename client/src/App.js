da5e4dc6d4fffb7b6f3a19bf40e7764e24426e6b
Devante W <williams.devante05@gmail.com>
2021-03-18 20:09:13 -0500

    creat react app and build out components and models

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+import Navbar from "./components/Navbar";
+import Card from "./components/Card"
+
+
+function App() {
+  return (
+    <div className="container">
+      <Navbar />
+      <Card/>
+
+    </div>
+  );
+}
+
+export default App;
