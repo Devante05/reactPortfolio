34586b9cd3f530adfc697a22c57d51c6308b56a1
Devante W <williams.devante05@gmail.com>
2021-03-18 20:32:44 -0500

    add form

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import Form from "./components/Form"
 import Navbar from "./components/Navbar";
 import Card from "./components/Card"
 
@@ -8,6 +8,7 @@ function App() {
   return (
     <div className="container">
       <Navbar />
+      <Form/>
       <Card/>
 
     </div>
---
+++ client/src/components/Form/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Form() {
+  return (<div className= "form-container col-lg-3">
+    <input type = "text" className= "form-control"></input>
+    <input type = "text" className= "form-control"></input>
+    <input type = "text" className= "form-control"></input>
+    <button type = "submit" className= "btn btn-primary">submit </button>
+
+  </div>
+
+  );
+}
+
+export default Form;
